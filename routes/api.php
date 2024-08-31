6bde441b84ad2d072fab9597b76ca9af63aa22b1
zobirofkir <zobirofkir19@gmail.com>
2024-08-31 23:26:33 +0100

    Add Comments From api.php

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,56 @@ use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 
 
+/**
+ * Auth routes
+ */
 Route::post("/auth/register", [AuthController::class, "register"]);
 Route::post("/auth/login", [AuthController::class, "login"]);
 
 
+/**
+ * Password reset
+ */
 Route::get('password/reset', function () {
     return view('emails.reset-password');
 })->name('password.reset.form');
-
 Route::post('password/email', [PasswordResetController::class, 'sendResetLinkEmail'])->name('password.email');
-
 Route::post('password/reset', [PasswordResetController::class, 'reset'])->name('password.reset');
 
+
+
+/**
+ * API routes
+ */
 Route::middleware("auth:api")->group(function() {
+
+    /**
+     * User routes
+     */
     Route::apiResource("/auth/users", UserResource::class);
+
+    /**
+     * Logout Route
+     */
     Route::post("/auth/logout", [AuthController::class, "logout"]);
+
+    /**
+     * Me Route
+     */
     Route::get("auth/me", [AuthController::class, "me"]);
+    
+    /**
+     * Refresh Route
+     */
     Route::post("/auth/refresh", [AuthController::class, "refresh"]);
+
+    /**
+     * Update Route
+     */
     Route::post("/auth/update", [AuthController::class, "update"]);
+
+    /**
+     * Destroy Route
+     */
     Route::post("/auth/destroy", [AuthController::class, "destroy"]);
 });
